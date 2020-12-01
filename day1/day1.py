10f1b536a4971bcd89d642bad18de2cdce8ca1fb
Aileen <aileen.tran27@gmail.com>
2020-12-01 12:28:27 -0800

    Completed part2

--- day1/day1.py
+++ day1/day1.py
@@ -24,7 +24,7 @@ def multiply_sums(nums):
         expense_idx[num] = idx
     return "Nothing sums to 2020!"
 
-print(multiply_sums(expense_nums))
+# print(multiply_sums(expense_nums))
 # run time: O(n) bc fast lookup w/ dictionary
 # run space: O(n) -> dictionary length at worst is same length as list
 
@@ -35,7 +35,23 @@ output: num - find 3 entries that sum to 2020 and multiply them
 """
 
 def multiply_three_sums(nums):
-    nums.sort()
-    print(nums)
+    expense_idx = {}
+    for idx1 in range(0, len(nums) - 1):
+        num1 = nums[idx1]
+        for idx2 in range(idx1 + 1, len(nums)):
+            num2 = nums[idx2]
+            diff = 2020 - num1 - num2
+            if diff <= 0:
+                continue
+            if diff in expense_idx.keys():
+                return diff * num1 * num2
+            expense_idx[num2] = idx2
+        expense_idx[num1] = idx1
+    return "There are NOT 3 nums that sum to 2020"
 
-# print(multiply_three_sums(expense_nums))
+example = [1721, 979, 366, 299, 675, 1456] #979, 366, 675
+print(multiply_three_sums(expense_nums))
+# print(multiply_three_sums(example))
+
+# run time: n = len(nums), l = len(n - 1) --> O(n * l) ~ O(n^2)
+# run space: worse case, O(n)
