278b6ac55dfa71e293446e4c39fcde09bed02123
Aileen <aileen.tran27@gmail.com>
2020-12-01 11:48:15 -0800

    Optimized part 1. Setting up part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,4 @@
-"""
-Day 1: Report Repair
-
-input: list of numbers
-output: num - find 2 entries that sum to 2020 and multiply them
-"""
+"""Day 1: Report Repair"""
 # opening file and converting text to ints!
 expense = open("day1_input.txt", "r", encoding="utf-8")
 expense_str = expense.read().split('\n') #an empty string at end of list
@@ -15,12 +10,30 @@ expense_nums = []
 for string in expense_str:
     num = int(string)
     expense_nums.append(num)
-
+"""
+Part 1
+input: list of numbers
+output: num - find 2 entries that sum to 2020 and multiply them
+"""
 def multiply_sums(nums):
-    for num in nums:
+    expense_idx = {}
+    for idx, num in enumerate(nums):
         diff = 2020 - num
-        if diff in nums:
+        if diff in expense_idx.keys():
             return num * diff
+        expense_idx[num] = idx
     return "Nothing sums to 2020!"
 
 print(multiply_sums(expense_nums))
+
+"""
+Part 2
+input: list of numbers
+output: num - find 3 entries that sum to 2020 and multiply them
+"""
+
+def multiply_three_sums(nums):
+    nums.sort()
+    print(nums)
+
+# print(multiply_three_sums(expense_nums))
