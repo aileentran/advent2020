1585201175c5679e03ba0008036b4cb0154d5345
Aileen <aileen.tran27@gmail.com>
2020-12-03 15:41:15 -0800

    Initial solution

--- day3/day3.py
+++ day3/day3.py
@@ -14,7 +14,9 @@ input notes - oh my god the input is ONE pattern..
 how many patterns do we need?
 3 right, 1 down
 orrr loop through the same pattern and keep track of where to start next
-
+grid[row][col]
+row = grid idx
+col = string idx
 """
 def open_file(file_name):
     pattern = list()
@@ -29,6 +31,31 @@ map = open_file('input.txt')
 example = open_file('example.txt')
 
 def toboggan_trajectory(grid):
-    return
+    height = len(grid)
+    width = len(grid[0])
+    row = col = 0
+    tree_counter = 0
+    # the end is where row_idx = height - 1
+    print('height', height)
+    print('width', width)
+    while row < height - 1:
+        if col < width - 3:
+            col += 3
+            row += 1
+            # print(grid[row])
+            # print('row', row)
+            # print('col', col)
 
-print(toboggan_trajectory(example))
+            pos = grid[row][col]
+            if pos == '#':
+                # print('TREE!')
+                tree_counter += 1
+
+            # print('POSITION', pos)
+            # print('\n')
+        else:
+            # print('You have reached the edge!')
+            col = col - width
+    return tree_counter
+
+print(toboggan_trajectory(map))
