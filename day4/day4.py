937025e7a01e3ba927be7027bc49e9f12e12a542
Aileen <aileen.tran27@gmail.com>
2020-12-04 11:03:06 -0800

    Initial commit

---
+++ day4/day4.py
@@ -0,0 +1,19 @@
+"""
+Day 4: Passport Processing
+input: convert file to dictionary w/ key:value pairs
+output: int - number of valid passports
+
+Part 1
+valid passport = need to have everything except for cid (country id)
+cid is optional or else.. we be stranded
+"""
+
+def open_file(file_name):
+    # file = open(file_name, 'r')
+    return
+
+def passport_processing(passport):
+    return
+
+# print(passport_processing()) example!
+# print(passport_processing()) actual input!
