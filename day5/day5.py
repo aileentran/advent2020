69d0fe91ddaa1e7a2990eb42f6a162b01b18b7c9
Aileen <aileen.tran27@gmail.com>
2020-12-05 15:23:19 -0800

    Initial commit

---
+++ day5/day5.py
@@ -0,0 +1,17 @@
+"""
+Day 5: Binary Boarding
+
+Part 1
+input: string - 10 chars
+output: num - seat id = row * 8 + col
+
+Notes:
+first 7 chars indicate which specific row
+last 3 chars indicate which col in row = seat
+plane: 128 rows, 8 cols of seats
+
+Example and explanation
+BFFFBBFRRR: row 70, column 7, seat ID 567.
+FFFBBBFRRR: row 14, column 7, seat ID 119.
+BBFFBBFRLL: row 102, column 4, seat ID 820.
+"""
