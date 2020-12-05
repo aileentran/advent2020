9a71c4c211182c3e7a0efd00800408e09528320b
Aileen <aileen.tran27@gmail.com>
2020-12-05 15:32:56 -0800

    Func to open file and set up main func

--- day5/day5.py
+++ day5/day5.py
@@ -2,7 +2,7 @@
 Day 5: Binary Boarding
 
 Part 1
-input: string - 10 chars
+input: list of strings - 10 chars
 output: num - seat id = row * 8 + col
 
 Notes:
@@ -15,3 +15,20 @@ BFFFBBFRRR: row 70, column 7, seat ID 567.
 FFFBBBFRRR: row 14, column 7, seat ID 119.
 BBFFBBFRLL: row 102, column 4, seat ID 820.
 """
+
+def open_file(file_name):
+    file = open(file_name, 'r')
+    binary = []
+    for line in file:
+        line = line.rstrip('\n')
+        binary.append(line)
+    return binary
+
+example = open_file('example.txt')
+input = open_file('input.txt')
+
+def binary_boarding(binary):
+    return
+
+print(binary_boarding(example))
+# print(binary_boarding(input))
