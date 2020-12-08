f48662420e5f71f128528229ddda6fde9332f0e6
Aileen <aileen.tran27@gmail.com>
2020-12-08 07:29:57 -0800

    Initial commit. WIP Part 1

---
+++ day8/day8.py
@@ -0,0 +1,27 @@
+"""
+Day 8: Handheld Halting
+There is an infinite loop
+What value is the accumulator before second loop?
+input: maybe list of tuples? (action, value)
+output: num - accumulator
+"""
+def open_file(file_name):
+    file = open(file_name, 'r')
+    instructions = []
+    for line in file:
+        line = line.rstrip('\n')
+        line = line.split(' ')
+        instruction_str, num_str = line
+        num = int(num_str)
+        instructions.append((instruction_str, num))
+    return instructions
+example = open_file('example.txt')
+input = open_file('input.txt')
+
+def accumulator_value(instructions):
+    index_dict = {}
+    for instruction in instructions:
+        print(instructions)
+
+print(accumulator_value(example))
+# print(accumulator_value(input))
