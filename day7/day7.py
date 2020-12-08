f9b591990e51064621bd9495b8efe2fd2c45de91
Aileen <aileen.tran27@gmail.com>
2020-12-07 16:29:37 -0800

    Added test cases to example and helper function. WIP

--- day7/day7.py
+++ day7/day7.py
@@ -28,6 +28,7 @@ def bag_and_contents(rule):
     bag = {}
     outer, contents = rule
     outer = outer.rstrip(' ')
+    outer = outer.rstrip('s')
     contents = contents.split(',')
     has_contents = {}
     for content in contents:
@@ -38,7 +39,6 @@ def bag_and_contents(rule):
             bag[outer] = content
             return bag
         else:
-            # specifics = {}
             bag_info = ' '.join(info[1:])
             bag_info = bag_info.rstrip('s') #make it easy to search bag later?
             has_contents[bag_info] = int(amount)
@@ -48,11 +48,45 @@ def bag_and_contents(rule):
 
 def handy_haversacks(rules):
     dict_rules = rules_to_dictionary(rules)
-    bag_colors = 0
+    holds_target = set()
+    seen = 0
+    colors = 0
+    holds_shiny = directly_holds_shiny(rules)
+    print('directly has shiny', holds_shiny)
+    # print(len(holds_target))
+    # count bags that hold bags that hold shiny gold bag
+    # 4 of loops below to reach answer..
     for rule in dict_rules:
-        print(rule)
-        # print(dict_rules[rule])
-        # contents = dict_rules[bag]
-        # print('bag', bag, contents)
+      # print('rule', rule)
+      for bag in rule:
+          # print(rule[bag])
+          contents = rule[bag]
+          if contents == 'no other bags':
+              continue
+          else:
+              contents = contents.keys()
+          for content in contents:
+              if content in holds_target:
+                  holds_target.add(bag)
+    # print(holds_target)
+    # print(len(holds_target))
+    return len(holds_target)
 
+def directly_holds_shiny(rules):
+    dict_rules = rules_to_dictionary(rules)
+    holds_shiny = set()
+    for rule in dict_rules:
+        for bag in rule:
+            contents = rule[bag]
+            if contents == 'no other bags':
+                continue
+            contents = contents.keys()
+            if 'shiny gold bag' in contents:
+                holds_shiny.add(bag)
+    return holds_shiny
+
+
+# somehow check for bags that hold bags that hold shiny gold bag? degrees removed!
+# seems to be able to catch degrees removed from shiny gold bag
 print(handy_haversacks(example))
+# print(handy_haversacks(input))
