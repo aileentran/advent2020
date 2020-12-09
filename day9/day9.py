3dee1887f98c2e9f255edf078e31b6428d7d101f
Aileen <aileen.tran27@gmail.com>
2020-12-09 12:35:17 -0800

    WIP Part 1

--- day9/day9.py
+++ day9/day9.py
@@ -13,3 +13,23 @@ example.txt rules:
 5 number preamble that moves down the list
 6th number = sum of any 2 nums in the preamble
 """
+def open_file(file_name):
+    file = open(file_name, 'r')
+    nums = []
+    for line in file:
+        line = int(line.rstrip('\n'))
+        nums.append(line)
+    return nums
+example = open_file('example.txt')
+input = open_file('input.txt')
+
+def encoding_error(nums, preamble):
+    i = 0
+    while i < len(nums) - preamble:
+        preamble_nums = nums[i:i + preamble]
+        sum_num = nums[i + preamble]
+        print(sum_num)
+        i += 1
+
+print(encoding_error(example, 5))
+# print(encoding_error(input, 25))
