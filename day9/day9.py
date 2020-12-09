4cc5eaa3ec50e133e2fe5f5cfd0e5b610c81d6e2
Aileen <aileen.tran27@gmail.com>
2020-12-09 15:17:25 -0800

    Completed Part 2

--- day9/day9.py
+++ day9/day9.py
@@ -31,11 +31,30 @@ def encoding_error(nums, preamble):
         for p_idx, num in enumerate(preamble_nums):
             diff = sum_num - num
             if p_idx == len(preamble_nums) - 1 and diff not in preamble_nums:
-                print('not a sum num!')
                 return sum_num
             if diff in preamble_nums:
                 break
         i += 1
 
 # print(encoding_error(example, 5))
-print(encoding_error(input, 25))
+# print(encoding_error(input, 25))
+
+"""
+Part 2
+find contiguous set of 2+ nums that sum to nonencrypted from part 1
+input: list of numbers
+output: num - sum smallest and largest in contiguous range
+"""
+def encryption_weakness(nums, preamble):
+    error = encoding_error(nums, preamble)
+    for i in range(len(nums) - preamble):
+        sum_error = []
+        for k in range(i, i + preamble):
+            summing_list = sum(sum_error)
+            if summing_list != error:
+                sum_error.append(nums[k])
+            else:
+                return min(sum_error) + max(sum_error)
+
+# print(encryption_weakness(example, 5))
+print(encryption_weakness(input, 25))
