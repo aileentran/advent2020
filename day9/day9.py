4e22794706de8b5a786ebdd731b812eb59c16e83
Aileen <aileen.tran27@gmail.com>
2020-12-09 10:54:04 -0800

    Initial commit

---
+++ day9/day9.py
@@ -0,0 +1,15 @@
+"""
+Day 9: Encoding Error
+
+Part 1
+input: list of numbers
+output: num - the first number that does not follow rules
+
+input.txt rules:
+25 number preamble that moves down the list
+26th num = sum of any 2 nums in preamble
+
+example.txt rules:
+5 number preamble that moves down the list
+6th number = sum of any 2 nums in the preamble
+"""
