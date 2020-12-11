ad117054a519d303d382f2cb5cdebf3672d6b4e5
Aileen <aileen.tran27@gmail.com>
2020-12-11 10:29:54 -0800

    Cleaned up comments

--- day10/day10.py
+++ day10/day10.py
@@ -12,8 +12,8 @@ def open_file(file_name):
     for line in file:
         line = line.rstrip('\n')
         adapters.append(int(line))
-    # outlet = 0
-    # adapters.append(outlet)
+    outlet = 0
+    adapters.append(outlet)
     adapters.sort()
     device = adapters[-1] + 3
     adapters.append(device)
@@ -43,57 +43,23 @@ Part 2
 input: list of adapters (including outlet and device)
 output: num of ways it can be arranged
 """
-"""
-# adapter_arrangments([0], input_text)
-def adapter_arrangements2(left, right):
-    count = 0
-    appendings = []
+tribonnaci_sequence = [1, 1, 2, 4, 7, 13, 24, 44, 81, 149]
 
-    if left[-1] + 3 == highest+3var
-        return 1
-    end
+def tribonnaci(num):
+    if num > len(tribonnaci_sequence):
+        return f"Can't calculate %{num}"
+    return tribonnaci_sequence[num - 1]
 
-    right.each do |a|
-        if left.last + 3 <= a
-            appendings.append(a)
-        end
-    end
+def adapter_arrangements(adapters):
+    current = multiplier = 1
+    for adapter in adapters:
+        if adapter + 1 in adapters:
+            current += 1
+        else:
+            multiplier *= tribonnaci(current)
+            current = 1
+    return multiplier
 
-    appendings.each do |a|
-        count += adapter_arrangements2(left + a, next_right.pluck(a))
-    end
-
-    return count
-"""
-
-def adapter_arrangements(left, right, device):
-    left_copy = left.copy()
-    right_copy = right.copy()
-    count = 0
-    appendings = []
-    print(left)
-    if left_copy[-1] == device:
-        # print('base case', left_copy)
-        # print('leftmost num', left_copy[-1])
-        # print('base case device', device)
-        # print(left_copy[-1] == device)
-        return 1
-
-    for adapter in right_copy:
-        # print(left_copy[-1], adapter)
-        if abs(left_copy[-1] - adapter) <= 3:
-            appendings.append(adapter)
-    # print('appendings', appendings)
-    for appending in appendings:
-        # print('appending', appending, appendings)
-        # print('left', left_copy, len(left_copy))
-        # print('right', right_copy)
-        right_copy.remove(appending)
-        left_copy.append(appending)
-        count += adapter_arrangements(left_copy, right_copy, device)
-
-    return count
-
-# print(adapter_arrangements([0], example1, example1[-1]))
-# print(adapter_arrangements([0], example2, example2[-1]))
-print(adapter_arrangements([0], input, input[-1]))
+# print(adapter_arrangements(example1))
+# print(adapter_arrangements(example2))
+print(adapter_arrangements(input))
