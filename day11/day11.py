bdb76c05891f48844d1500b01d1b813114aa7b5e
Aileen <aileen.tran27@gmail.com>
2020-12-15 10:45:18 -0800

    Completed valid_adj. WIP find_final

--- day11/day11.py
+++ day11/day11.py
@@ -23,18 +23,35 @@ def open_file(file_name):
 example = open_file('example.txt')
 input = open_file('input.txt')
 
+def valid_adjacent(curr, width, height):
+    r_idx, c_idx = curr
+    potential_adj = [(r_idx - 1, c_idx - 1), (r_idx - 1, c_idx), (r_idx - 1, c_idx + 1),
+    (r_idx, c_idx - 1), (r_idx, c_idx + 1),
+    (r_idx + 1, c_idx - 1), (r_idx + 1, c_idx), (r_idx + 1, c_idx + 1)]
+    adj = []
+    for potential in potential_adj:
+        r, c = potential
+        if r >= 0 and c >= 0 and r < height and c < width:
+            adj.append(potential)
+    return adj
+
 def finding_final(layout):
     copy = layout.copy()
     width = len(copy[0])
     height = len(copy)
     final = copy
 
-    print(width)
-    print(height)
-    for row in copy:
-        print('r', row)
-        # for col in row:
-            # print('col', col)
+    for r_idx, row in enumerate(copy):
+        for c_idx, col in enumerate(row):
+            curr = (r_idx, c_idx)
+            adj = valid_adjacent(curr, width, height)
+            print('curr', curr, copy[r_idx][c_idx])
+            for a in adj:
+                r_adj, c_adj = a
+                seat = copy[r_adj][c_adj]
+                print(a, seat)
+
+
     return
 
 print(finding_final(example))
