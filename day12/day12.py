b9de1afa0c5be0500962732fd1d2798facc5ac3a
Aileen <aileen.tran27@gmail.com>
2020-12-16 13:37:20 -0800

    Completed Part 1

--- day12/day12.py
+++ day12/day12.py
@@ -29,4 +29,36 @@ def open_file(file_name):
 example = open_file('example.txt')
 input = open_file('input.txt')
 # print(example)
-print(input)
+# print(input)
+
+def distance(instructions, starting_dir):
+    compass = ['N', 'E', 'S', 'W'] #right: idx + 1 % 4?, left: -1
+    start = starting_dir
+    east_west = 0
+    north_south = 0
+    for ins in instructions:
+        dir, val = ins
+        compass_idx = compass.index(start)
+        turn = val // 90
+        if dir == 'R':
+            start = compass[(compass_idx + turn) % 4]
+            dir = start
+            continue
+        if dir == 'L':
+            start = compass[compass_idx - turn]
+            dir = start
+            continue
+
+        if dir == 'F':
+            dir = start
+        if dir == 'N':
+            north_south += val
+        elif dir == 'S':
+            north_south -= val
+        elif dir == 'E':
+            east_west += val
+        elif dir == 'W':
+            east_west -= val
+    return abs(east_west) + abs(north_south)
+# print(distance(example, 'E'))
+# print(distance(input, 'E'))
