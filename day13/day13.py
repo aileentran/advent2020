79ea6cd48363101ce3d9f05e50250a7c4b161095
Aileen <aileen.tran27@gmail.com>
2020-12-21 15:29:34 -0800

    Created open_file function. Parsed input data properly.

---
+++ day13/day13.py
@@ -0,0 +1,31 @@
+"""
+Day 13: Shuttle Search
+
+Part 1:
+all buses depart airport at 0 min
+all buses return to airport at ID min
+
+input: time, list of bus IDs and x for buses NOT running
+output: ID * min to wait for the bus
+"""
+
+def open_file(file_name):
+    file = open(file_name, 'r')
+    info = []
+    bus_ids = []
+    for line in file:
+        line = line.rstrip('\n')
+        if line.isdigit() == True:
+            info.append(int(line)) #this is the time of arrival!
+        else:
+            ids = line.split(',')
+            for id in ids:
+                if id.isdigit() == True:
+                    id = int(id)
+                    # consider ONLY including valid bus ids?
+                bus_ids.append(id)
+    info.append(bus_ids)
+    return info
+example = open_file('example.txt')
+input = open_file('input.txt')
+print(example)
